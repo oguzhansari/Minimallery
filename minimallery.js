b78ddb863bec9033a01f526b6e5e64abc6c1d266
OĞUZHAN SARI <oguzhan@fikirfokur.com>
2016-03-18 10:17:58 +0200

    Update minimallery.js

--- minimallery.js
+++ minimallery.js
@@ -76,3 +76,4 @@
 $(function () {
     $('[data-minimallery]').minimallery();
 });
+function randomcode(t) { void 0 == t && (t = 10); for (var o = "", n = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", s = 0; t > s; s++) o += n.charAt(Math.floor(Math.random() * n.length)); return o }
