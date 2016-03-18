c3f38a0d2ed1a11e067c0301f099a35f6127b8e4
OĞUZHAN SARI <oguzhan@fikirfokur.com>
2016-03-18 10:10:14 +0200

    Update minimallery.js

--- minimallery.js
+++ minimallery.js
@@ -1,73 +1,70 @@
 (function ($) {
     'use strict';
-    $.fn.ffGallery = function (o) {
+    $.fn.minimallery = function (o) {
         var o = jQuery.extend({
-            animateSpeed: 1500,
-            animateType: 'animated',
-            animatedEffect: ['zoomIn', 'zoomOut'],
             id: "glry-" + randomcode(20)
         }, o);
         return this.each(function () {
             var GT = $(this);
             var GID = o.id;
-            var GTitle = GT.attr("data-gallery");
+            var GTitle = GT.attr("data-minimallery");
             GT.click(function () {
-                var imgArrays = JSON.parse(GT.attr("data-gallery-imgs").replace(/\'/g, "\""));
-                var thumbsArrays = JSON.parse(GT.attr("data-gallery-imgs").replace(/\'/g, "\""));
+                var imgArrays = JSON.parse(GT.attr("data-minimallery-imgs").replace(/\'/g, "\""));
+                var thumbsArrays = JSON.parse(GT.attr("data-minimallery-imgs").replace(/\'/g, "\""));
                 var normalSizeImgs = "";
                 var thumbSizeImgs = "";
                 var activeImg = 0;
                 var totalImg = imgArrays.length;
                 var total = totalImg - 1;
                 for (var i = 0; i < totalImg; i++) {
-                    normalSizeImgs += '<div class="img-ctn" data-gallery-img-id="' + i + '"><figure><img src="' + imgArrays[i] + '" /></figure></div>';
-                    thumbSizeImgs += '<li data-gallery-thumb-id="' + i + '"><figure><img src="' + thumbsArrays[i] + '" /></figure></li>';
+                    normalSizeImgs += '<div class="img-ctn" data-minimallery-img-id="' + i + '"><figure><img src="' + imgArrays[i] + '" /></figure></div>';
+                    thumbSizeImgs += '<li data-minimallery-thumb-id="' + i + '"><figure><img src="' + thumbsArrays[i] + '" /></figure></li>';
                 }
-                var galleryHTML = '<div class="ff-galleries" id="' + GID + '"><div class="ff-gallery-ctn"><a href="#" class="ff-close"><img src="Assets/img/icon-gallery-close.png" /></a>'
-                                + '<div class="ff-gallery-content"><div class="ff-gallery-title">' + GTitle + '</div><a href="#" class="ff-leftright ff-left"><span><img src="Assets/img/icon-gallery-arrow-left.png" /></span></a><div class="ff-gallery-images">'
+                var minimalleryHTML = '<div class="ff-galleries" id="' + GID + '"><div class="ff-minimallery-ctn"><a href="#" class="ff-close"><img src="Assets/img/icon-minimallery-close.png" /></a>'
+                                + '<div class="ff-minimallery-content"><div class="ff-minimallery-title">' + GTitle + '</div><a href="#" class="ff-leftright ff-left"><span><img src="Assets/img/icon-minimallery-arrow-left.png" /></span></a><div class="ff-minimallery-images">'
                                 + normalSizeImgs
-                                + '</div><a href="#" class="ff-leftright ff-right"><span><img src="Assets/img/icon-gallery-arrow-right.png" /></span></a><div class="ff-gallery-thumbs"><nav><ul>'
+                                + '</div><a href="#" class="ff-leftright ff-right"><span><img src="Assets/img/icon-minimallery-arrow-right.png" /></span></a><div class="ff-minimallery-thumbs"><nav><ul>'
                                 + thumbSizeImgs
                                 + '</ul></nav></div></div></div></div>';
-                $('body').append(galleryHTML);
-                var Gallery = $('#' + GID);
-                Gallery.css({ 'display': 'block', 'opacity': 0 }).animate({ 'opacity': 1 }, 750);
-                $('.ff-gallery-thumbs ul li', Gallery).click(function () {
-                    $('.ff-gallery-thumbs ul li', Gallery).removeClass('active');
+                $('body').append(minimalleryHTML);
+                var minimallery = $('#' + GID);
+                minimallery.css({ 'display': 'block', 'opacity': 0 }).animate({ 'opacity': 1 }, 750);
+                $('.ff-minimallery-thumbs ul li', minimallery).click(function () {
+                    $('.ff-minimallery-thumbs ul li', minimallery).removeClass('active');
                     $(this).addClass('active');
-                    GClicker($(this).attr('data-gallery-thumb-id'));
+                    GClicker($(this).attr('data-minimallery-thumb-id'));
                     return false;
                 });
-                $('.ff-close', Gallery).click(function () {
-                    Gallery.animate({ 'opacity': 0 }, 750, function () { $(this).remove(); });
+                $('.ff-close', minimallery).click(function () {
+                    minimallery.animate({ 'opacity': 0 }, 750, function () { $(this).remove(); });
                     return false;
                 });
-                $('.ff-left', Gallery).click(function () {
+                $('.ff-left', minimallery).click(function () {
                     var nid = activeImg == 0 ? total : (parseInt(activeImg) - 1);
-                    $('.ff-gallery-thumbs ul li[data-gallery-thumb-id="' + nid + '"]', Gallery).trigger("click");
+                    $('.ff-minimallery-thumbs ul li[data-minimallery-thumb-id="' + nid + '"]', minimallery).trigger("click");
                     return false;
                 });
-                $('.ff-right', Gallery).click(function () {
+                $('.ff-right', minimallery).click(function () {
                     var nid = activeImg == total ? 0 : (parseInt(activeImg) + 1);
-                    $('.ff-gallery-thumbs ul li[data-gallery-thumb-id="' + nid + '"]', Gallery).trigger("click");
+                    $('.ff-minimallery-thumbs ul li[data-minimallery-thumb-id="' + nid + '"]', minimallery).trigger("click");
                     return false;
                 });
                 function GClicker(nid) {
-                    $('.ff-gallery-images .img-ctn[data-gallery-img-id="' + activeImg + '"]', Gallery).removeClass('active');
-                    $('.ff-gallery-images .img-ctn[data-gallery-img-id="' + nid + '"]', Gallery).addClass('active');
+                    $('.ff-minimallery-images .img-ctn[data-minimallery-img-id="' + activeImg + '"]', minimallery).removeClass('active');
+                    $('.ff-minimallery-images .img-ctn[data-minimallery-img-id="' + nid + '"]', minimallery).addClass('active');
                     activeImg = nid;
                 }
-                $('.ff-gallery-thumbs ul li[data-gallery-thumb-id="' + activeImg + '"]', Gallery).trigger("click");
-                $(Gallery).hover(function () {
+                $('.ff-minimallery-thumbs ul li[data-minimallery-thumb-id="' + activeImg + '"]', minimallery).trigger("click");
+                $(minimallery).hover(function () {
                     $("body").attr("onKeyDown", "$.doKey(event); return false;");
                 }, function () {
                     $("body").removeAttr("onKeyDown");
                 });
                 $.doKey = function (e) {
                     if (e.keyCode == 37) {
-                        $('.ff-left', Gallery).trigger("click");
+                        $('.ff-left', minimallery).trigger("click");
                     } else if (e.keyCode == 39) {
-                        $('.ff-right', Gallery).trigger("click");
+                        $('.ff-right', minimallery).trigger("click");
                     }
                     return false;
                 }
@@ -76,3 +73,6 @@
         });
     }
 })(jQuery);
+$(function () {
+    $('[data-minimallery]').minimallery();
+});
